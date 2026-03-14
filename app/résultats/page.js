3bd0856dffa27fd41e7b4369cda6da73a4055b04
Francis <your@email.com>
2026-03-13 20:06:15 -0400

    résultats

--- app/résultats/page.js
+++ app/résultats/page.js
@@ -145,6 +145,87 @@ export default function Résultats() {
            <div className="max-w-6xl mx-auto bg-black/60 border border-red-600/40 backdrop-blur-md p-10 rounded-2xl shadow-xl">
 
             <h1 className="text-2xl font mb-7 tracking-wide">
+              March Madness IX - 22 mars 2025
+              Centre Multifonctionnel, Saint-Ephrem
+            </h1>
+            
+            <img
+              src="/Images/resultats_mars2025.png"
+              alt="Résultats March Madness IX"
+              className="mx-auto rounded-2xl shadow-xl w-full max-w-4xl object-contain"
+            />
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+01 Pee Wee bat Buxx Belmar avec quelques tactiques déloyales durant le combat. Ce qui en a surpris plus d'un.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+02 Dreya Mitchell accompagnée de Carloss l'emporte sur Leon Saver.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+Le Icon Club a tenté d'intervenir dans le match. Cependant, ça n'a pas porté fruit puisqu'ils ont été expulsés par l'arbitre, mais également pourchassés par Carloss qui tenait à défendre sa belle Dreya.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+03 SeXXXy Eddy est déclaré grand gagnant du Lucky 7 Scramble, où il remporte la grande somme de 5000 $. Il bat Jason Petitclerc, Jeremy Prophet, Chester Doan, Jonny Rush et Muundo.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+04 Kristara a le dessus sur Mckenrose dans un combat qui a su démontrer que la TUW a une forte division féminine.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+Carloss Gonzales s'amène au ring et confie à Dreya Mitchell qu'il a des sentiments pour elle. Dans un revirement inattendu, elle l'accueille d'une botte au visage pour ensuite nous avouer qu'elle l'a manipulé pendant deux mois pour obtenir la seule chose qu'elle voulait vraiment de lui : sa ceinture UWD.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+Elle nous rappelle qu'elle a gagné son droit de combattre pour ce championnat lorsqu'elle a remporté son combat en janvier lors d'Original Kickoff. Un arbitre était déjà sur place pour officialiser le combat qui allait opposer Carloss à un adversaire mystère, qui se trouve à être Dreya.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+05 Dreya Mitchell se sauve avec la victoire contre Carloss en quelques secondes seulement et devient la nouvelle championne Ultimate Wrestling Division. Pauvre Carloss.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+06 Mr. Brown et Barabas gagnent contre Nitzz et Bjorg ainsi que le Icon Club (Mitch et DGenerate) pour devenir les aspirants numéro 1 aux titres par équipe de la TUW.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+07 Scott Matrix l'emporte sur Radikal Rage dans un combat où Tony Icon était l'arbitre spécial.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+Grâce à cette victoire, Scott Matrix se mérite une chance de mettre la main au collet de Tony Icon dans un combat un contre un. Un affrontement auquel Tony Icon ne doit pas avoir hâte.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+08 Le Capitaine Pezz et Jake Madison s'amènent au ring. On était tous impatients de savoir qui était l'équipe mystère qui allait affronter ce duo. Le Capitaine lui-même nous annonçait une surprise majeure.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+C'est alors que la musique et la vidéo des Hardy Boyz se font entendre. Les gens sur place n'en croyaient pas leurs yeux. Mais au final, c'était une ruse de la part du Capitaine Pezz qui s'en est pris sauvagement à son partenaire Jake Madison.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+La sécurité a dû intervenir pour sortir Pezz de la salle. Qu'est-ce qui a pu motiver le capitaine à agir ainsi ? Histoire à suivre.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+09 Ensuite avait lieu le combat de championnat vacant de la TUW entre Chase Ironside et Ryan Rogan, qui ont été interrompus en fin de combat par Pee Wee. Il a attendu le moment parfait pour s'imposer au match et encaisser sa valise "Ultimate Cash In" pour finalement remporter le championnat TUW. Incroyable.
+</p>
+
+<p className="text-gray-300 text-lg leading-relaxed mb-6">
+10 Les Invincibles l'emportent contre TDT dans un solide combat non sanctionné par la TUW. Ce combat n'était pas pour les doux, c'est le moins qu'on puisse dire. Un candidat au combat de l'année.
+</p>
+          </div>
+        </section>
+
+        
+        <section className="pt-40 pb-24 px-6 text-center">
+           <div className="max-w-6xl mx-auto bg-black/60 border border-red-600/40 backdrop-blur-md p-10 rounded-2xl shadow-xl">
+
+            <h1 className="text-2xl font mb-7 tracking-wide">
               Prime Attraction 2025 - 22 février 2025
               Centre Multifonctionnel, Saint-Ephrem
             </h1>
@@ -189,7 +270,6 @@ Une grande nouvelle est sortie durant la soirée. Rhino, ancienne vedette de la 
           </div>
         </section>
 
-        
         <section className="pt-40 pb-24 px-6 text-center">
            <div className="max-w-6xl mx-auto bg-black/60 border border-red-600/40 backdrop-blur-md p-10 rounded-2xl shadow-xl">
 
